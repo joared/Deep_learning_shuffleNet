22a6387ec6d1751b29e86766e906bd961111585f
Joar Edlund <joared@kth.se>
2019-05-19 21:32:04 +0200

    bug

--- shufflenet_model_builder.py
+++ shufflenet_model_builder.py
@@ -57,11 +57,11 @@ def shufflenet_model_cifar10_small(name, input_image, group, shuffle):
 				4:[272, 544, 1088],
 				8:[384, 768, 1536]}
 	#l = shufflenet_unit("sh1", l, channels[group][0], group, 2)
-	l = shufflenet_unit_v2("sh2", l, channels[group][0], group, 1, shuffle)
-	l = shufflenet_unit("sh3", l, channels[group][0], group, 1, shuffle)
-	l = shufflenet_unit("sh4", l, channels[group][0], group, 1, shuffle)
+	#l = shufflenet_unit_v2("sh2", l, channels[group][0], group, 1, shuffle)
+	#l = shufflenet_unit("sh3", l, channels[group][0], group, 1, shuffle)
+	#l = shufflenet_unit("sh4", l, channels[group][0], group, 1, shuffle)
 	
-	l = shufflenet_stage("stage_1", l, channels[group][1], 3, group, shuffle)
+	l = shufflenet_stage("stage_1", l, channels[group][1], 5, group, shuffle)
 	
 	l = tf.layers.flatten(l)
 	l = tf.layers.dense(l, 10, use_bias=True)
